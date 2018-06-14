2e2e20c6b298f16af505daf44673820803294315
Dowon Cha <dowoncha.cs@gmail.com>
2018-06-14 13:57:08 -0400

    add react native vector icons dependency
    remove react native button components
    
    link react-native vector icons

--- android/app/src/main/java/com/demos/MainApplication.java
+++ android/app/src/main/java/com/demos/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
@@ -77,6 +78,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNGoogleSigninPackage(),
             new ReactNativeYouTube(),
             new LinearGradientPackage(),
