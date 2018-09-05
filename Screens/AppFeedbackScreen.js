deb0fe0ac9d3da54dba4fc02b8a6bb4ea4c64b68
izzihinks <ihinks@live.unc.edu>
2018-09-05 00:19:58 -0400

    styled feedback screen

--- Screens/AppFeedbackScreen.js
+++ Screens/AppFeedbackScreen.js
@@ -40,12 +40,36 @@ export default class AppFeedbackScreen extends React.Component {
   render() {
     return (
       <ScrollView>
+      <View style={{alignItems: 'center', justifyContent: 'center', marginTop: 20, marginLeft: 20, marginRight: 20}}>
+      <Image
+      style={{width: 120, height: 120, marginTop: 20, marginBottom: 20}}
+      source={{uri: 'https://user-images.githubusercontent.com/18129905/35187343-734d21b4-fdf0-11e7-8799-761570dea412.png'}}
+      />
+      <View style={{height: 20}}></View>
+      <Text>The main purpose of Acta is to increase the transparency and communication between you and your local government. We intend for this app to serve as an accessible, easy-to-use outlet to empower you with information and let your voice be heard by the decision-makers.
+      </Text>
+      </View>
+      <View style={{height: 20}}></View>
+      <View style={{alignItems: 'center', justifyContent: 'center', marginLeft: 20, marginRight: 20}}>
+      <Text>We want your feedback! What do you like and dislike about the app? How can we improve it to fit your individual needs and support your voice?
+      </Text>
+      <View style={{height: 20}}></View>
         <TextInput
-        placeholder='Put your feedback on the app here'
+        placeholder='   Please add your feedback here'
+        style={{marginLeft: 19, marginRight: 19, backgroundColor: '#DFDFE2', borderRadius: 3}}
         editable={true}
         multiline={true}
         onChangeText={this.storeFeedback}/>
+        <View style={{height: 10}}></View>
+        <View style={{marginLeft: 30, marginRight: 30, marginTop: 5, marginBottom: 5, borderRadius: 5, alignItems: 'center', backgroundColor: '#55CFE0', paddingTop: 3, paddingBottom: 3, paddingLeft: 3, paddingRight: 3}}>
         <TouchableOpacity onPress={this.submit}><Text>Submit feedback</Text></TouchableOpacity>
+        </View>
+        <View style={{height: 20}}></View>
+        <Image
+            style={{height: 45, width: 80}}
+            source={{ uri: 'https://user-images.githubusercontent.com/18129905/43050902-6fc94544-8dde-11e8-94fd-9bdfa1df6ead.png'}}
+        />
+        </View>
       </ScrollView>
     )
   }
