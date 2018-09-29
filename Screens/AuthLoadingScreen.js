9b47cf1dae47bf1fd32d5f23499ca50416fca7b6
Andrew McKinnon <andrewmckinnon2@gmail.com>
2018-09-29 11:11:38 -0400

    Fixed account creation error message and RegistrationScreen1 scrollview

--- Screens/AuthLoadingScreen.js
+++ Screens/AuthLoadingScreen.js
@@ -16,14 +16,15 @@ class AuthLoadingScreen extends React.Component {
         // Else redirect to authentication router
 
         //Needs to be returned to AsyncStorage.getItem('user')
-        let user = await AsyncStorage.getItem('user')
+        let user = await AsyncStorage.getItem('use')
 
         if (!user) {
             console.log("No user session")
             user = await firebaseApp.auth().currentUser;
 
             if (user) {
-                this.saveItem('user', user)
+              //need to change back to user
+                this.saveItem('use', user)
             }
         } else {
             console.log("User found in session", user.uid)
--- Screens/RegistrationScreen1.js
+++ Screens/RegistrationScreen1.js
@@ -28,38 +28,6 @@ export default class RegistrationScreen1 extends Component {
     }
   }
 
-  //On submit verify inputs and navigate to next registration screen
-  submit = () => {
-    //Validate inputs
-    //TODO: Improve data check and search database to see if this username already exists
-    if(this.state.phone == ''){
-      this.state.phone = "none"
-    }
-
-    if(this.state.name != '' && this.state.username != '' && this.state.password != '' && this.state.email != ''){
-      //Check and make sure user with this email is not already on file
-      this.usersRef.once("value").then((snap) => {
-
-        if(snap.hasChild(sha1(this.state.email))){
-          Alert.alert("this email is already on file");
-        }else{
-          //Make a new node and populate with correct info about the user, aquired from this page
-          newUserPush = this.usersRef.child(sha1(this.state.email)).set({
-            name: this.state.name,
-            password: sha1(this.state.password),
-            email: this.state.email,
-            phone: this.state.phone,
-            username: this.state.username,
-          });
-          //Once new user has been written, navigate to RegistrationScreen2 with the username passed as arg to maintain correct reference in database
-          this.props.navigation.navigate('RegistrationScreen2', {hashemail: sha1(this.state.email)});
-        }
-      });
-    }else{
-      Alert.alert("Please ensure that all fields are filled in.");
-    }
-  };
-
   onSubmit = async () => {
     // Submit's current information to firebase
     // Is name and phone numbber necessary
@@ -88,10 +56,12 @@ export default class RegistrationScreen1 extends Component {
       //this.props.navigation.navigate('AuthLoading')
 
     } catch (error) {
+      console.log("inside of error catch");
       // TODO: Proper error handling
       const {code, message}  = error;
-      console.error(code, message);
-      Alert.alert("There was an error in submission, please check your email and password and try again");
+      //console.error(code, message);
+      console.log("after the console.error call");
+      Alert.alert("Please check your email and password and try again - Password must be at least 6 characters");
     }
   }
 
@@ -113,31 +83,35 @@ export default class RegistrationScreen1 extends Component {
   render() {
     console.disableYellowBox = true;
     return (
-      <View style={{flex: 1}}>
-        <View style={styles.container}>
-          <Image
-          style={{width: 200, height: 200, marginTop: 20, marginBottom: 20}}
-          source={{uri: 'https://user-images.githubusercontent.com/18129905/35187343-734d21b4-fdf0-11e7-8799-761570dea412.png'}}
-          />
+        <ScrollView style={{flex: 1}}>
+          <View style={styles.container}>
+            <Image
+            style={{width: 200, height: 200, marginTop: 20, marginBottom: 20}}
+            source={{uri: 'https://user-images.githubusercontent.com/18129905/35187343-734d21b4-fdf0-11e7-8799-761570dea412.png'}}
+            />
+          </View>
+          <View>
+          <View>
+          <TextInput style={{marginTop: 15, marginBottom: 10, marginLeft: 75, marginRight: 75, backgroundColor: '#FFFFFF', fontSize: 13, borderRadius: 3}} onChangeText={this.handleName}placeholder="name"/>
+          <TextInput style={{marginTop: 15, marginBottom: 10, marginLeft: 75, marginRight: 75, backgroundColor: '#FFFFFF', fontSize: 13, borderRadius: 3}} onChangeText={this.handleEmail} placeholder="email"/>
+          <TextInput secureTextEntry={true} style={{marginTop: 15, marginBottom: 10, marginLeft: 75, marginRight: 75, backgroundColor: '#FFFFFF', fontSize: 13, borderRadius: 3}} onChangeText={this.handlePassword} placeholder="password"/>
+          <TextInput style={{marginTop: 15, marginBottom: 10, marginLeft: 75, marginRight: 75, backgroundColor: '#FFFFFF', fontSize: 13, borderRadius: 3}} onChangeText={this.handlePhone} placeholder="phone number (optional)"/>
+          <Text style={{marginTop: 15, alignItems: 'center', textAlign: 'center', marginLeft: 75, marginRight: 75}}>Please make sure that your password is at least 6 characters long.</Text>
         </View>
-        <View>
-        <View>
-        <TextInput style={{marginTop: 15, marginBottom: 10, marginLeft: 75, marginRight: 75, backgroundColor: '#FFFFFF', fontSize: 13, borderRadius: 3}} onChangeText={this.handleName}placeholder="name"/>
-        <TextInput style={{marginTop: 15, marginBottom: 10, marginLeft: 75, marginRight: 75, backgroundColor: '#FFFFFF', fontSize: 13, borderRadius: 3}} onChangeText={this.handleEmail} placeholder="email"/>
-        <TextInput secureTextEntry={true} style={{marginTop: 15, marginBottom: 10, marginLeft: 75, marginRight: 75, backgroundColor: '#FFFFFF', fontSize: 13, borderRadius: 3}} onChangeText={this.handlePassword} placeholder="password"/>
-        <TextInput style={{marginTop: 15, marginBottom: 10, marginLeft: 75, marginRight: 75, backgroundColor: '#FFFFFF', fontSize: 13, borderRadius: 3}} onChangeText={this.handlePhone} placeholder="phone number (optional)"/>
-        <Text style={{marginTop: 15, alignItems: 'center', textAlign: 'center', marginLeft: 75, marginRight: 75}}>Please make sure that your password is at least 6 characters long.</Text>  
-      </View>
-        <View style={{height: 32}}></View>
-        <Button onPress={this.onSubmit} title="Register" />
+          <View style={{height: 32}}></View>
+          <View style={styles.buttonz}>
+            <TouchableOpacity onPress={this.onSubmit}>
+              <Text style={{fontSize: 16, textAlign: 'center'}}>Submit and Continue</Text>
+            </TouchableOpacity>
+          </View>
 
-        {/* <View style={styles.buttonz}>
-          <TouchableOpacity onPress={this.onSubmit}>
-            <Text style={{fontSize: 16}}>Continue</Text>
-          </TouchableOpacity>
-        </View> */}
-        </View>
-      </View>
+          {/* <View style={styles.buttonz}>
+            <TouchableOpacity onPress={this.onSubmit}>
+              <Text style={{fontSize: 16}}>Continue</Text>
+            </TouchableOpacity>
+          </View> */}
+          </View>
+        </ScrollView>
     );
   }
 }
@@ -187,6 +161,7 @@ var styles = StyleSheet.create({
     height: 40,
     borderRadius: 4,
     width: 320,
+    marginBottom: 30,
     backgroundColor: '#49C7E3',
     alignItems: 'center',
     justifyContent: 'center',
